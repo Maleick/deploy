3ee94b4c0e17d25be2d01db80ca8e3895b9786f1
Maleick <maleick@icloud.com>
2021-01-14 04:03:13 -0600

    Add pip3

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Author: Maleick
-# Version: 1.50
-# Update: 01/10/21
+# Version: 1.60
+# Update: 01/14/21
 # Deploy Kali VMWare image setup
 
 cat << "EOF"
@@ -23,7 +23,7 @@ white=$'\e[0m'
 # Updates
 echo "$green Deploying Updates $white"
 apt update
-apt install at bc bloodhound build-essential chromium dkms dnsmasq gss-ntlmssp powershell hostapd mingw-w64 openjdk-11-jdk seclists wine32 -y
+apt install at bc bloodhound build-essential chromium dkms dnsmasq hostapd gss-ntlmssp powershell python3-pip mingw-w64 openjdk-11-jdk seclists wine32 -y
 apt full-upgrade -y
 apt autoremove
 
@@ -33,15 +33,12 @@ pip install mitm6 pypykatz
 
 # Clone all the things
 echo "$green Deploy the Clones $white"
-git clone https://github.com/SecureAuthCorp/impacket.git /opt/impacket; cd /opt/impacket; pip install .
+git clone https://github.com/SecureAuthCorp/impacket.git /opt/impacket; cd /opt/impacket; pip3 install .
 git clone https://github.com/FortyNorthSecurity/Egress-Assess.git /opt/Egress-Assess
 git clone https://github.com/PowerShellMafia/PowerSploit.git /opt/PowerSploit
-git clone https://github.com/fox-it/BloodHound.py.git /opt/BloodHound.py; cd /opt/BloodHound.py; pip install .
 git clone https://github.com/s0lst1c3/eaphammer.git /opt/eaphammer
-git clone https://github.com/josephkingstone/cobalt_strike_extension_kit.git /opt/cobalt_strike_extension_kit
 git clone https://github.com/carlospolop/privilege-escalation-awesome-scripts-suite.git /opt/winPEAS
 git clone https://github.com/lgandx/Responder.git /opt/Responder
-git clone --recursive https://github.com/eteran/edb-debugger.git /opt/edb-debugger
 gem install evil-winrm
 
 # Setup SSH
