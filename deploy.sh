51f6e297989e5d51aaaa569799232c3ea75b1b28
Maleick <maleick@icloud.com>
2021-01-15 00:00:32 -0600

    Parrot is the new Kali

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # Author: Maleick
-# Version: 1.60
-# Update: 01/14/21
-# Deploy Kali VMWare image setup
+# Version: 1.62
+# Update: 01/15/21
+# Deploy Parrot image setup
 
 cat << "EOF"
 ██████╗ ███████╗██████╗ ██╗      ██████╗ ██╗   ██╗   ███████╗██╗  ██╗
@@ -23,20 +23,19 @@ white=$'\e[0m'
 # Updates
 echo "$green Deploying Updates $white"
 apt update
-apt install at bc bloodhound build-essential chromium dkms dnsmasq hostapd gss-ntlmssp powershell python3-pip mingw-w64 openjdk-11-jdk seclists wine32 -y
+apt install at bc build-essential gss-ntlmssp powershell mingw-w64 openjdk-11-jdk seclists -y
 apt full-upgrade -y
 apt autoremove
 
 # Pip install
 echo "$green Deploying Pip $white"
-pip install mitm6 pypykatz
+pip3 install mitm6 pypykatz
 
 # Clone all the things
 echo "$green Deploy the Clones $white"
 git clone https://github.com/SecureAuthCorp/impacket.git /opt/impacket; cd /opt/impacket; pip3 install .
 git clone https://github.com/FortyNorthSecurity/Egress-Assess.git /opt/Egress-Assess
 git clone https://github.com/PowerShellMafia/PowerSploit.git /opt/PowerSploit
-git clone https://github.com/s0lst1c3/eaphammer.git /opt/eaphammer
 git clone https://github.com/carlospolop/privilege-escalation-awesome-scripts-suite.git /opt/winPEAS
 git clone https://github.com/lgandx/Responder.git /opt/Responder
 gem install evil-winrm
